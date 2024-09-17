7ebe83ae6e0d35a2d612a537c252cf1456088f63
Виктор Лобынцев <sibfight@mail.ru>
2024-09-17 23:37:05 +0300

    literalsType

---
+++ advType/literalsType.ts
@@ -0,0 +1,13 @@
+function fetchWithAuth(url: string, method: 'POST' | 'GET'): 1 | -1 {
+    url
+    method
+    return 1
+}
+
+fetchWithAuth('s', 'POST')
+// fetchWithAuth('s', '123')
+
+let a1: 'asdasd' = 'asdasd'
+
+const method = 'POST'
+fetchWithAuth('s', method)
