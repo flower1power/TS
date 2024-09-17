204fa79cb82677f477cdbe910ae39b3ebe5c9f9e
Виктор Лобынцев <sibfight@mail.ru>
2024-09-17 20:48:38 +0300

    array and tuples

---
+++ baseType/array.ts
@@ -0,0 +1,12 @@
+const skills: string[] = ['Dev', 'DevOps', 'QA']
+
+for (const skill of skills) {
+    console.log(skill.toUpperCase())
+}
+
+const a = skills
+    .filter((s: string) => s !== 'DevOps')
+    .map(s => s + '! ')
+    .reduce((a, b) => a + b)
+
+console.log(a)
---
+++ baseType/readonly.ts
@@ -0,0 +1,6 @@
+//! Нельзя модифицировать
+const skills3: readonly [number, string] = [1, 'QA']
+
+const skills4: readonly string[] = ['DEV', 'QA']
+
+const skills5: ReadonlyArray<string> = ['DEV', 'QA']
