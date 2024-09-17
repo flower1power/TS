aa1c16e88d505540742b0a6f01a8ad0e7926a2bf
Виктор Лобынцев <sibfight@mail.ru>
2024-09-17 23:13:02 +0300

    task2

---
+++ baseType/task2.ts
@@ -0,0 +1,41 @@
+/* Запрос */
+// {
+//     "topicId": 5,
+//     "status": "published" // "draft", "deleted"
+// }
+
+
+// /* Ответ */
+// [
+//     {
+//         "question": "Как осуществляется доставка?",
+//         "answer": "быстро!",
+//         "tags": [
+//             "popular",
+//             "new"
+//         ],
+//         "likes": 3,
+//         "status": "published"
+//     }
+// ]
+
+const enum Status {
+    PUBLISHED = 'published',
+    DRAFT = 'draft',
+    DELETED = 'deleted',
+}
+
+async function getFaqs(req: { topicId: number, status?: Status }): Promise<{
+    question: string,
+    answer: string,
+    tags: string[],
+    likes: number,
+    status?: Status
+}[]> {
+    const res = await fetch('/faqs', {
+        method: 'POST',
+        body: JSON.stringify(req)
+    });
+    
+    return await res.json();
+}
