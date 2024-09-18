cbc1fcb46c77b99a8b9b0e43c7efbaa77f5abef4
Виктор Лобынцев <sibfight@mail.ru>
2024-09-18 16:54:29 +0300

    Опциональные типы

---
+++ advType/optional.ts
@@ -0,0 +1,36 @@
+interface User7 {
+    login: string;
+    password?: string;
+}
+
+const user7: User7 = {
+    login: 'admin',
+}
+
+function multiply(first: number, second?: number): number {
+    if (!second) {
+        return first * first
+    }
+
+    return first * second;
+}
+
+multiply(5)
+multiply(5, 5)
+
+
+interface UserPro {
+    login: string;
+    password?: {
+        type: 'primary' | 'secondary'
+    };
+}
+
+function testPass(user: UserPro) {
+    return user.password?.type
+}
+
+function test(param?: string) {
+    // если param null || undefined то multiply
+    return param ?? multiply(5)
+}
