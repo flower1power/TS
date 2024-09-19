b2eed54358d191691a69e3e4d24a4ba1957aa13e
Виктор Лобынцев <sibfight@mail.ru>
2024-09-19 16:49:57 +0300

    never

---
+++ advType/never.ts
@@ -0,0 +1,39 @@
+//* Функция никогда не вернется
+function generateError(msg: string): never {
+  throw new Error(msg);
+}
+
+function dumpError(): never {
+  while (true) {
+  }
+}
+
+function rec(): never {
+  return rec();
+}
+
+type paymentAction = 'refund' | 'checkout' | 'reject'
+
+function processAction(action: paymentAction) {
+  switch (action) {
+    case 'refund':
+      break;
+    case 'checkout':
+      break;
+    default:
+      // прикольная проверка при сужении
+      // const _: never = action;
+      throw new Error('Нет такого action');
+  }
+}
+
+
+function isString(x: string | number): boolean {
+  if (typeof x === 'string') {
+    return true;
+  } else if (typeof x === 'number') {
+    return false;
+  }
+
+  generateError('qqq');
+}
