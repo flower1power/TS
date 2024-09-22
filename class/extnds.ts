58540773edd769a518aa7c950f762b9a6e7a9f40
Виктор Лобынцев <sibfight@mail.ru>
2024-09-22 14:31:04 +0300

    дополнение

--- class/extnds.ts
+++ class/extnds.ts
@@ -35,3 +35,88 @@ class PersistedPayment extends Payment2 {
 }
 
 new PersistedPayment();
+
+class User22 {
+  name: string = 'user22';
+
+  constructor() {
+    console.log(this.name);
+  }
+}
+
+class Admin2 extends User22 {
+  name: string = 'Admin';
+
+  constructor() {
+    super();
+    console.log(this.name);
+  }
+}
+
+new Admin2();
+
+class HttpError extends Error {
+  conde: number;
+
+  constructor(mess: string, code: number) {
+    super(mess);
+    this.conde = code ?? 500;
+  }
+}
+
+
+// Плохой пример наследования
+// class User23 {
+//   name: string = 'user23';
+//
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+//
+//   override toString() {
+//     return this.map(u => u.name).join(' ');
+//   }
+// }
+//
+// class Users extends Array<User23> {
+//   searchByName(name: string) {
+//     return this.filter(user => user, name === name);
+//   }
+// }
+
+// Норм пример
+
+class User23 {
+  name: string = 'user23';
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
+
+
+class User24List {
+  users: User23[];
+
+  constructor(name: User23) {
+    this.users.push(name);
+  }
+}
+
+class Payment3 {
+  data: Date;
+}
+
+
+class UserWithPayment2 {
+  user: User23;
+  payment: Payment3;
+
+  constructor(user: User23, payment: Payment3) {
+    this.user = user;
+    this.payment = payment;
+  }
+}
+
+//? используем наследование если в одной доменной области
+//* не надо наследоваться от массовых методах
--- class/implementation.ts
+++ class/implementation.ts
@@ -1,33 +1,33 @@
-interface ILogger {
-  log(...args): void;
-  error(...args): void;
-}
+// interface ILogger {
+//   log(...args): void;
+//   error(...args): void;
+// }
 
-//? Класс реализовывает конкретный Интерфейс
-class Logger implements ILogger {
-  log(...args: any[]): void {
-    console.log(args);
-  }
-  async error(...args: any[]): Promise<void> {
-    console.log(args);
-  }
-}
+// //? Класс реализовывает конкретный Интерфейс
+// class Logger implements ILogger {
+//   log(...args: any[]): void {
+//     console.log(args);
+//   }
+//   async error(...args: any[]): Promise<void> {
+//     console.log(args);
+//   }
+// }
 
-interface IPayable {
-  pay(pamentId: number): void;
-  price?: number;
-}
+// interface IPayable {
+//   pay(pamentId: number): void;
+//   price?: number;
+// }
 
-interface IDeletable {
-  dalete(): void;
-}
+// interface IDeletable {
+//   dalete(): void;
+// }
 
-class User21 implements IPayable, IDeletable {
-  dalete(): void {
-    throw new Error("Method not implemented.");
-  }
-  pay(pamentId: number): void {
-    ///
-  }
-  price?: number | undefined;
-}
+// class User21 implements IPayable, IDeletable {
+//   dalete(): void {
+//     throw new Error("Method not implemented.");
+//   }
+//   pay(pamentId: number): void {
+//     ///
+//   }
+//   price?: number | undefined;
+// }
