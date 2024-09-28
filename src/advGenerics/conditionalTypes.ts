62e502d85b592e4f4eb6d5443d2803266c212b1a
Виктор Лобынцев <sibfight@mail.ru>
2024-09-28 13:42:35 +0300

    conditionalTypes

---
+++ src/advGenerics/conditionalTypes.ts
@@ -0,0 +1,51 @@
+const a3: number = Math.random() > 0.5 ? 1 : 0;
+
+interface HHTPResponse<T extends 'success' | 'failed'> {
+  code: number;
+  data: T extends 'success' ? string : Error;
+  // additinialData: T extends 'success' ? string : number;
+}
+
+const suc: HHTPResponse<'success'> = {
+  code: 200,
+  data: 'done',
+};
+
+const err: HHTPResponse<'failed'> = {
+  code: 200,
+  data: new Error(),
+};
+
+class User25 {
+  id: number;
+  name: string;
+}
+
+class UserPersistend extends User25 {
+  dbId: string;
+}
+
+function getUser(Id: number): User25;
+function getUser(dbId: string): UserPersistend;
+function getUser(dbIdOrId: string | number): User25 | UserPersistend {
+  if (typeof dbIdOrId === 'number') {
+    return new User25();
+  } else {
+    return new UserPersistend();
+  }
+}
+
+type User25OrUserPersistend<T extends string | number> = T extends string
+  ? UserPersistend
+  : User25;
+
+function getUser2<T extends string | number>(id: T): User25OrUserPersistend<T> {
+  if (typeof id === 'number') {
+    return new User25() as User25OrUserPersistend<T>;
+  } else {
+    return new UserPersistend() as User25OrUserPersistend<T>;
+  }
+}
+
+getUser2(1)
+getUser2('')
